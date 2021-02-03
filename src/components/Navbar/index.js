86790831a70a72aae12573a05fd15b1fc2a1ce8d
結城明日奈 <yuukixasuna00@gmail.com>
2021-02-03 03:29:32 -0300

    update cards

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -30,7 +30,7 @@ function Navbar() {
       <nav className="navbar">
         <div className="navbar-container">
           <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
-            ANIME<i className="fas fa-cat" />
+            TR<i className="fas fa-leaf" />VEL
           </Link>
           <div className="menu-icon" onClick={handleClick}>
             <i className={click ? "fas fa-times" : "fas fa-bars"} />
