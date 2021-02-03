8f4b0dc63c32b905e0cc1d493047aa3669ef8868
結城明日奈 <yuukixasuna00@gmail.com>
2021-02-03 02:44:34 -0300

    cards

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import './App.css';
 import Home from './components/pages/Home';
+import './App.css';
 
 function App() {
   return (
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Button } from "../Button";
-import "./Navbar.css";
+import './Navbar.css';
+
 
 function Navbar() {
   const [click, setClick] = useState(false);
@@ -29,7 +30,7 @@ function Navbar() {
       <nav className="navbar">
         <div className="navbar-container">
           <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
-            TRVL <i className="fab fa-typo3" />
+            ANIME<i className="fas fa-cat" />
           </Link>
           <div className="menu-icon" onClick={handleClick}>
             <i className={click ? "fas fa-times" : "fas fa-bars"} />
