1eac332526bf9b6bc8a0f985aec861f48775b501
結城明日奈 <yuukixasuna00@gmail.com>
2021-02-03 04:19:35 -0300

    add pages: services, products and sign-up

---
+++ src/components/pages/Products.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../../App.css";
+
+export default function Products() {
+  return (
+    <>
+      <video src='/videos/video-4.mp4' autoPlay loop muted />
+      <h1 className="products">PRODUCTS</h1>
+    </>
+  );
+}
---
+++ src/components/pages/SignUp.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../../App.css";
+
+export default function SignUp() {
+  return (
+    <>
+      <video src='/videos/video-3.mp4' autoPlay loop muted />
+      <h1 className="sign-up">SIGN UP</h1>;
+    </>
+  );
+}
